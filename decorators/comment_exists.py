6bdf4e8305252455efbc1273b582a8525801d5a3
Andrew Burkhart <BurkhartFC@iCloud.com>
2017-01-21 14:00:08 -0700

    remove questions.py as it is unnecessary and fix template dest

--- decorators/comment_exists.py
+++ decorators/comment_exists.py
@@ -12,11 +12,11 @@ from string import letters
 from google.appengine.ext import db
 
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+template_dir = os.path.join(os.path.dirname(__file__), '../templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
-                               
+
 def comment_exists(function):
     def wrapper(self, post_id):
         key = db.Key.from_path('Post', int(post_id))
